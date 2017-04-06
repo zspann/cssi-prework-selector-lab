8020ceaca92e5c7161c6d2a4da76595420082bfa
Zack Spann <zspann@zspann-macbookpro3.roam.corp.google.com>
2017-04-06 14:38:36 -0700

    Two tests not working but this is basically just a regurgitation of stackoverflow

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,26 @@ function pSelector(){
   return $('p');
 }
 */
+
+function h1Selector() {
+  return $('h1');
+}
+
+function liInOlSelector() {
+  return $('ol li')
+}
+
+// I think the test for this function is broken
+function linkSelector() {
+  return $('a#my-id.my-class')
+}
+
+
+// Maybe it's just me at this point but I think the test here is broken too
+function catSelector() {
+  return $('img[alt="cat sleeping"]')
+}
+
+function checkboxInputSelector() {
+  return $(':checkbox')
+}
